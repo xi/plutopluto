96d8af4e0a1b7b1aca33f9da8ec3e5d0da82a5b1
Tobias Bengfort <tobias.bengfort@posteo.de>
2023-07-01 21:19:52 +0200

    refactor js

--- plutopluto/static/plutopluto.js
+++ plutopluto/static/plutopluto.js
@@ -1,119 +1,124 @@
-"use strict";
+var template = document.querySelector('template');
+var stream = document.querySelector('#stream');
+var loading = document.querySelector('.loading');
 
-$(document).ready(function() {
-	$.ajax('/config', {success: function(config) {
-		var entries = [];
-		var page = 0;
+var formatDate = function(text) {
+	var date = new Date(parseInt(text, 10) * 1000);
+	var format = 'YYYY/MM/dd hh:mm';
 
-		// load templates
-		var template = $('#stream').html();
-		Mustache.parse(template);
-		$('#stream').html('');
+	var o = {
+		'Y': date.getFullYear(),
+		'M': date.getMonth() + 1,
+		'd': date.getDate(),
+		'h': date.getHours(),
+		'm': date.getMinutes(),
+		's': date.getSeconds,
+	};
 
-		var formatDate = function(date, format) {
-			var o = {
-				'Y': date.getFullYear(),
-				'M': date.getMonth() + 1,
-				'd': date.getDate(),
-				'h': date.getHours(),
-				'm': date.getMinutes(),
-				's': date.getSeconds
+	for (var key in o) {
+		if (new RegExp('(' + key + '+)').test(format)) {
+			var value = o[key].toString();
+			if (RegExp.$1.length > 1) {
+				value = ('0000' + value).substr(-RegExp.$1.length);
 			}
+			format = format.replace(RegExp.$1, value);
+		}
+	}
 
-			for (var key in o) {
-				if (new RegExp('(' + key + '+)').test(format)) {
-					var value = o[key].toString();
-					if (RegExp.$1.length > 1) {
-						value = ("0000" + value).substr(-RegExp.$1.length);
-					}
-					format = format.replace(RegExp.$1, value)
-				}
-			}
+	return format;
+};
 
-			return format;
-		};
+var bottomDistance = function() {
+	var doc = document.body.scrollHeight;
+	var screen = window.innerHeight;
+	var position = document.body.scrollTop || window.scrollY;
+	return doc - position - screen;
+};
 
-		var formatDateFilter = function() {
-			return function(text, render) {
-				var dt = new Date(parseInt(render(text), 10) * 1000);
-				return formatDate(dt, 'YYYY/MM/dd hh:mm');
-			}
-		};
+var escapeHTML = function(text) {
+	var p = document.createElement('p');
+	p.textContent = text;
+	return p.innerHTML;
+};
 
-		var bottomDistance = function() {
-			var doc = document.body.scrollHeight;
-			var screen = window.innerHeight;
-			var position = document.body.scrollTop || window.scrollY;
-			return doc - position - screen;
-		};
+var appendEntries = function(entries) {
+	entries.forEach(entry => {
+		var li = document.createElement('li');
+		li.className = 'post';
+		li.innerHTML = template.innerHTML
+			.replace('{{source}}', escapeHTML(entry.source))
+			.replace('{{link}}', escapeHTML(entry.link))
+			.replace('{{title}}', escapeHTML(entry.title))
+			.replace('{{dt}}', escapeHTML(formatDate(entry.dt)))
+			.replace('{{{content}}}', entry.content);
+		stream.append(li);
+	});
+};
 
-		var appendEntries = function(entries) {
-			entries.forEach(function(entry) {
-				entry.formatDate = formatDateFilter;
-				var rendered = Mustache.render(template, entry);
-				$('#stream').append($(rendered));
-			});
-		};
-
-		var loadNextPageLock = false;
-		var loadNextPage = function() {
-			if (!loadNextPageLock) {
-				loadNextPageLock = true;
-
-				config.urls.forEach(function(url) {
-					if (url.indexOf('{page}') >= 0) {
-						url = url.replace('{page}', page);
-					} else if (page !== 0) {
-						return;
-					}
-
-					$.ajax('/parse', {
-						data: {url: url},
-						success: function(data) {
-							entries = entries.concat(data.entries);
-
-							// now that we have entries, we can show some
-							if ($('#stream').children().length === 0) {
-								loadMore();
-							}
-
-							// ideally we would wait until all requests have finished
-							// but this is only a simple optimisation anyway
-							loadNextPageLock = false;
-						}
-					});
-				});
-				page++;
-			}
-		};
-
-		var loadMore = function() {
-			entries.sort(function(a, b) {
-				return b.dt - a.dt;
-			});
-			appendEntries(entries.splice(0, 10));
-			if (entries.length < 30) {
-				loadNextPage();
-			}
-		};
-
-		$(document).on('scroll', function() {
-			if (bottomDistance() < 4000) {
-				loadMore();
-			}
-		}, {passive: true});
-		$('.loading').click(loadMore);
-
-		// load initial content
-		loadNextPage();
-	}});
-
-	// youtube video embedding
-	$(document).on('click', function(event) {
-		if (event.target.alt.match(/^https:\/\/www\.youtube/)) {
-			var iframe = $('<iframe class="youtube">');
-			iframe.target.src = event.target.alt;
-			$(event.target).replace(iframe);
+var fetchJSON = function(url) {
+	return fetch(url).then(r => {
+		if (r.ok) {
+			return r.json();
+		} else {
+			throw r;
 		}
 	});
+};
+
+fetchJSON('/config').then(config => {
+	var entries = [];
+	var page = 0;
+
+	var loadNextPageLock = false;
+	var loadNextPage = function() {
+		if (loadNextPageLock) {
+			return;
+		}
+
+		loadNextPageLock = true;
+
+		var promises = config.urls.map(url => {
+			if (url.includes('{page}')) {
+				url = url.replace('{page}', page);
+			} else if (page !== 0) {
+				return;
+			}
+
+			return fetchJSON('/parse?' + new URLSearchParams({url: url})).then(data => {
+				entries = entries.concat(data.entries);
+			});
+		});
+
+		Promise.all(promises).finally(() => {
+			// now that we have entries, we can show some
+			if (stream.children.length === 0) {
+				loadMore();
+			}
+
+			loadNextPageLock = false;
+		});
+
+		page++;
+	};
+
+	var loadMore = function() {
+		entries.sort((a, b) => {
+			return b.dt - a.dt;
+		});
+		appendEntries(entries.splice(0, 10));
+		if (entries.length < 30) {
+			loadNextPage();
+		}
+	};
+
+	document.addEventListener('scroll', () => {
+		if (bottomDistance() < 4000) {
+			loadMore();
+		}
+	}, {passive: true});
+
+	loading.addEventListener('click', loadMore);
+
+	// load initial content
+	loadNextPage();
 });
